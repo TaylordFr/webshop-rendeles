d78bf1fc75477f94b983b2ec9fb70c60d6f969e6
Petrik Diak <diak@petrik.hu>
2024-10-10 15:12:32 +0200

    2024-10-10

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,17 +20,26 @@ export class AppController {
       nev: 'Curtis',
       szamlazasi_cim:'hun',
       szallitasi_cim:'hun',
-      kupon:'444',
-      bankkartya_szam:'4321-4567',
+      bankkartya:'4321-4567',
       lejarati_datum:'10/27',
+      kupon:'444',
       biztonsagi_kod: '999'
+    },
+    {
+      nev: 'Majka',
+      szamlazasi_cim: 'hun',
+      szallitasi_cim: 'hun',
+      bankkartya: '1234-5555',
+      lejarati_datum: '12/30',
+      kupon: '555',
+      biztonsagi_kod: '111'
     }
   ];
 
   
 
-  @Get('/insertdata')
-  @Render('ujAdat')
+  @Get('rendeles')
+  @Render('rendelesform')
   ujAdat(){
     return{
       errors: [],
@@ -38,23 +47,33 @@ export class AppController {
     }
   }
 
+  @Get('/rendelessuccess')
+  @Render('success')
+  getsuccess(){
+      return {
+        rendelesek: this.#rendelesek.length
+      }
+  }
 
-  @Get('/termekek')
-  @Render('termekek')
-  getTermekek() {
 
- }
-
- @Post('/insertdata')
+ @Post('rendeles')
  insertData(
   @Body() orderData: newRendeles,
     @Res() response: Response
  ){
     const errors: string[] = [];
 
-
     if(!orderData.nev || !orderData.szamlazasi_cim || !orderData.szallitasi_cim || !orderData.bankkartya || !orderData.lejarati_datum || !orderData.biztonsagi_kod){
       errors.push('Minden mezőt kötelező megadni!');
+    }
+    if(!/\d{2}\/d{2}$/.test(orderData.lejarati_datum) || this.Isexpired(orderData.lejarati_datum)){
+      errors.push('Helytelen a dátum!')
+    }
+    if(!/^\d{2}-\d{4}$/.test(orderData.kupon)){
+      errors.push('A kuponkód nem megfelelő formátumú!')
+    }
+    if(!/\d{4}-\d{4}$/.test(orderData.bankkartya)){
+      errors.push('A bankkártyaszám nem megfelelő formátumú!')
     }
     if(! /^\d{3}$/.test(orderData.biztonsagi_kod)){
       errors.push('A biztonsági kód nem megfelelő formátumú!')
@@ -63,19 +82,31 @@ export class AppController {
     const newRendeles = {
       nev: orderData.nev,
       szamlazasi_cim: orderData.szamlazasi_cim,
-      szallitasi_cim:orderData.szallitasi_cim,
-      kupon:orderData.kupon,
-      bankartya_szam:orderData.bankkartya,
-      lejarati_datum:orderData.lejarati_datum,
-      biztonsagi_kod:orderData.biztonsagi_kod
+      szallitasi_cim: orderData.szallitasi_cim,
+      bankkartya: orderData.bankkartya,
+      lejarati_datum: orderData.lejarati_datum,
+      kupon: orderData.kupon,
+      biztonsagi_kod: orderData.biztonsagi_kod
     }
 
-    if(errors.length < 0){
-      response.render('ujAdat', {
+    if(errors.length > 0){
+      response.render('rendelesform', {
         errors,
         data: orderData
       })
       return;
     }
+
+    this.#rendelesek.push(newRendeles)
+    response.redirect(303, '/rendelessuccess');
+
   }
+  
+
+    private Isexpired(lejarati_datum: string): boolean {
+        const  [month, year] = lejarati_datum.split('/').map(num => parseInt(num));
+        const now = new Date();
+        const expiry = new Date(2000 + year, month -1)
+        return expiry < now;
+    }
 }
